7aac645d782c33ff2b05544dda981c084597be35
StephenWalther <Stephen.Walther@superexpert.com>
2014-01-07 11:45:53 -0800

    Added sample of IValidatableObject interface

--- WebFormsScaffolding/ScaffoldingSampleOutput/WebFormsServerCRUD/Models/Movie.cs
+++ WebFormsScaffolding/ScaffoldingSampleOutput/WebFormsServerCRUD/Models/Movie.cs
@@ -6,7 +6,13 @@ using System.Web;
 
 namespace WebFormsServerCRUD.Models
 {
-    public class Movie
+
+    /// <summary>
+    /// This class is created by the user and it drives the scaffolding. Notice that you can use
+    /// both data annotation attributes and the IValidatableObject interface.
+    /// </summary>
+
+    public class Movie:IValidatableObject
     {
         public int Id { get; set; }
 
@@ -22,5 +28,15 @@ namespace WebFormsServerCRUD.Models
         [DataType(DataType.Currency)]
         [Required(ErrorMessage="You must enter a ticket price.")]
         public decimal TicketPrice { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var results = new List<ValidationResult>();
+            if (this.Director == "Stephen")
+            {
+                results.Add(new ValidationResult("Director named Stephen not allowed!"));
+            }
+            return results;
+        }
     }
 }
